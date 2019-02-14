41ac3886686faa35b3a88d7a9f4d3361ba308b42
Niikou <reidneko@gmail.com>
2019-02-13 23:07:07 -0500

    Forgot to do initial commits and onwards. So this is completed lab code.

--- app/views.py
+++ app/views.py
@@ -7,7 +7,7 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+import datetime
 
 ###
 # Routing for your application.
@@ -22,12 +22,19 @@ def home():
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="Neko S. C. Reid")
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html', joined=format_date_joined(datetime.datetime.now()))
 
 
 ###
 # The functions below should be applicable to all Flask apps.
 ###
+
+def format_date_joined(date):
+    return  date.strftime("%B, %Y") 
 
 @app.route('/<file_name>.txt')
 def send_text_file(file_name):
@@ -52,7 +59,6 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
-
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port="8080")
